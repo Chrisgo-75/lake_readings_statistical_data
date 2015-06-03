8a44e284aebf2f7d0b79cee624f9b6907e7b680a
Chris Arndt <carndt@cals.wisc.edu>
2015-06-03 09:27:10 -0500

    a) asked user for a date range. b) added validations to verify date range falls withins acceptable parameters.

--- functions.rb
+++ functions.rb
@@ -1,43 +1,88 @@
 require 'readline' # is a library that allows you to interact with the user to get user input.
 require 'date'
 
+# The earliest date for which there is consistent data
+DATA_START_DATE = '2006-09-20'
+
+# We want to be kind to the remote server. This is the maximum number
+# of days that can be retrieved. Remember for each day, we will make 3
+# queries, one for each reading type. Keep this value low.
+MAX_DAYS = 7
+
+
 # Ask the user (via the command line) to provide valid start and end dates.
 #
 # @return [array] [start_date, end_date]
 #
 def query_user_for_date_range
   start_date = nil
-  end_date = nil
+  end_date   = nil
 
-  puts "\nFirst, we need a start date."
-  start_date = query_user_for_date
+  # Until we have a start_date and end_date execute this loop.
+  until start_date && end_date
+    puts "\nFirst, we need a start date."
+    start_date = query_user_for_date
 
-  puts "\nNext, we need an end date."
-  end_date = query_user_for_date
+    puts "\nNext, we need an end date."
+    end_date = query_user_for_date
+
+    if !date_range_valid?(start_date, end_date)
+      puts "Let's try again."
+      start_date = end_date = nil
+    end
+  end # END until
 
   return [start_date, end_date]
 end
 
-# Ask the user (via the command line) for a single valid date. 
+# Ask the user (via the command line) for a single valid date.
 # Used for both start and end dates.
 def query_user_for_date
   date = nil
+
   until date.is_a? Date
     prompt = "Please enter a date as YYYY-MM-DD: "
     response = Readline.readline(prompt, true)
-    
+
     # We have the option to quit at any time.
     exit if ['quit', 'exit', 'q', 'x'].include?(response)
 
     begin
       date = Date.parse(response) # "parse" takes a string and tries to turn it into a Date Object.
-                                  # IF "parse" fails THEN it will return an argument error AND is
-                                  # why this parse is inside this BEGIN END RESCUE block which will
-                                  # RESCUE it if argument error and just output "Invalid date format."
+        # IF "parse" fails THEN it will return an argument error AND is
+        # why this parse is inside this BEGIN END RESCUE block which will
+        # RESCUE it if argument error and just output "Invalid date format."
     rescue ArgumentError
       puts "\nInvalid date format."
     end
-    
+
+    date = nil unless date_valid?(date)
+
   end
   return date
+end
+
+# Test if a single date is valid
+# We know that any single date (start_date or end_date) must be after the "DATA_START_DATE" and before today.
+def date_valid?(date)
+  valid_dates = Date.parse(DATA_START_DATE)..Date.today
+  # Does the "valid_dates" "cover" the passed in date?
+  if valid_dates.cover?(date)
+    return true
+  else
+    puts "\nDate must be after #{DATA_START_DATE} and before today."
+    return false
+  end
+end
+
+# Test if a range of dates is valid
+def date_range_valid?(start_date, end_date)
+  if start_date > end_date
+    puts "\nStart date must be before end date."
+    return false
+  elsif start_date + MAX_DAYS < end_date
+    puts "\nNo more than #{MAX_DAYS} days. Be kind to the remote server!"
+    return false
+  end
+  return true
 end
